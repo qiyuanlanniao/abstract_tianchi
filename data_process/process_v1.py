36853735a01ee8c791b857112060bf84d3e04762
qiyuanlanniao <2514776650@qq.com>
2024-06-01 08:35:50 +0800

    采用固定分配的train和val

--- data_process/process_v1.py
+++ data_process/process_v1.py
@@ -4,42 +4,52 @@
 
 import json
 import pandas as pd
-from sklearn.model_selection import train_test_split
-
-
-
 
 if __name__ == '__main__':
-    with open('data/train.json','r') as file:
+    # train
+    with open('data/train_data.json','r') as file:
         content = file.readlines()
         content = [line.strip() for line in content if line.strip()]
         data = [json.loads(line) for line in content]
-    out_df = []
+    train_df = []
     for item in data:
-        
+        events = item['events']
+        summarizations = item['summarizations']
+        assert len(events) == len(summarizations)
+        for idx , (event,summarization) in enumerate(zip(events,summarizations)):
+            train_df.append({
+                'source':event['content'],
+                'target':summarization['event-summarization'],
+            })
+    train_df = pd.DataFrame(train_df)
+
+    # val
+    with open('data/val_data.json','r') as file:
+        content = file.readlines()
+        content = [line.strip() for line in content if line.strip()]
+        data = [json.loads(line) for line in content]
+    val_df = []
+    for item in data:
         events = item['events']
         summarizations = item['summarizations']
         assert len(events) == len(summarizations)
 
         for idx , (event,summarization) in enumerate(zip(events,summarizations)):
-            out_df.append({
+            val_df.append({
                 'source':event['content'],
                 'target':summarization['event-summarization'],
             })
-    out_df = pd.DataFrame(out_df)
+    val_df = pd.DataFrame(val_df)
 
-    train,val = train_test_split(out_df,test_size=0.2,random_state=0)
-
-    print(f'训练集合 {train.shape}')
-    print(f'验证集合 {val.shape}')
+    print(f'训练集合 {train_df.shape}')
+    print(f'验证集合 {val_df.shape}')
     # 清洗空文本
-    train = train[train['source'].apply(lambda x:len(x)!=0 )]
-    train = train[train['target'].apply(lambda x:len(x)!=0 )]
-    val = val[val['source'].apply(lambda x:len(x)!=0 )]
-    val = val[val['target'].apply(lambda x:len(x)!=0 )]
-    print(f'清洗后 训练集合 {train.shape}')
-    print(f'清洗后 验证集合 {val.shape}')
+    train_df = train_df[train_df['source'].apply(lambda x:len(x)!=0 )]
+    train_df = train_df[train_df['target'].apply(lambda x:len(x)!=0 )]
+    val_df = val_df[val_df['source'].apply(lambda x:len(x)!=0 )]
+    val_df = val_df[val_df['target'].apply(lambda x:len(x)!=0 )]
+    print(f'清洗后 训练集合 {train_df.shape}')
+    print(f'清洗后 验证集合 {val_df.shape}')
 
-    train.to_csv('data/process_v1_train.csv',index=False)
-    val.to_csv('data/process_v1_val.csv',index=False)
-
+    train_df.to_csv('data/process_v1_train.csv',index=False)
+    val_df.to_csv('data/process_v1_val.csv',index=False)
