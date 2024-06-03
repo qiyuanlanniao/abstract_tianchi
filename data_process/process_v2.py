5336b4441efd8f2d5de28032906a7c380581bc81
1139069819 <1139069819@qq.com>
2024-06-03 22:06:03 +0800

    添加val的summarization作为验证集

--- data_process/process_v2.py
+++ data_process/process_v2.py
@@ -29,6 +29,30 @@ if __name__ == '__main__':
                 train_df.append({
                     'source':doc,
                     'target':doc})
-    print(f'2 . train_df {len(train_df)}')    
-            
+    print(f'2 . train_df {len(train_df)}')                
     train_df = pd.DataFrame(train_df)
+    train_df = train_df[train_df['source'].apply(lambda x:len(x)!=0 )]
+    train_df = train_df[train_df['target'].apply(lambda x:len(x)!=0 )]
+    train_df.to_csv('data/pretrain_train.csv',index=False)
+
+    # val
+
+
+    with open('data/val_data.json','r') as file:
+        content = file.readlines()
+        content = [line.strip() for line in content if line.strip()]
+        data = [json.loads(line) for line in content]
+    val_df = []
+    for item in data:
+        events = item['events']
+        summarizations = item['summarizations']
+        for idx , (event,summarization) in enumerate(zip(events,summarizations)):
+            val_df.append({
+                'source':summarization['event-summarization'],
+                'target':summarization['event-summarization'],
+            })
+    val_df = pd.DataFrame(val_df)
+    val_df = val_df[val_df['source'].apply(lambda x:len(x)!=0 )]
+    val_df = val_df[val_df['target'].apply(lambda x:len(x)!=0 )]
+    val_df.to_csv('data/pretrain_val.csv',index=False)
+    
